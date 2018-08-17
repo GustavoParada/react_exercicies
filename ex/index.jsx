deff30d1bee11ca22f62e2474fd4c1d15f5a4ce9
Gustavo <gustavoparada.net@gmail.com>
2018-08-17 00:43:12 -0300

    usando redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 import ReactDOM from 'react-dom';
 import Field from './fild'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import fieldReducer from './fieldReducer'
 
+
+const reducers = combineReducers({
+    field: fieldReducer
+})
 
 ReactDOM.render(
-    <div>
+    <Provider store={createStore(reducers)}>
         <Field initialValue='123'></Field>
-    </div>
+    </Provider>
     , document.getElementById('app'))
 
